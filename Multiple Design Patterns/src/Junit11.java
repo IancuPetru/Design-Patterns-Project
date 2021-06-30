df17cc2b762f54da2f2bf4c3fd5bae382683dae2
IancuPetru <84980308+IancuPetru@users.noreply.github.com>
2021-07-01 00:00:04 +0300

    Update Junit11.java

--- Multiple Design Patterns/src/Junit11.java
+++ Multiple Design Patterns/src/Junit11.java
@@ -6,9 +6,9 @@ class Junit11 {
 
 	@Test
 	void test() {
-		 Amenities H1= new foodbar(new Spa(new foodbar(new HotelRoom())));
+		 Amenities H1= new new Spa(new foodbar(new HotelRoom()));
 		  double output=H1.cost();
-		  assertEquals(90,output);
+		  assertEquals(80,output);
 	}
 
 }
